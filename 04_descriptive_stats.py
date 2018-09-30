541ce3d3be151cf32c392b538d4f4e2827879030
rodferro <rod_ferro@msn.com>
2018-09-30 16:56:25 -0300

    Add python script

---
+++ 04_descriptive_stats.py
@@ -0,0 +1,6 @@
+# calculate descriptive statistics
+from pandas import read_csv
+
+filename = 'daily-total-female-births.csv'
+series = read_csv(filename, header=0, index_col=0, parse_dates=True, squeeze=True)
+print(series.describe())
