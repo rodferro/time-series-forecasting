8e36496932b643ef08c66d575589d8d70a1a3b4e
rodferro <rod_ferro@msn.com>
2018-09-30 16:39:47 -0300

    Add python script

---
+++ 04_load_csv.py
@@ -0,0 +1,6 @@
+from pandas import read_csv
+
+filename = 'daily-total-female-births.csv'
+series = read_csv(filename, header=0, index_col=0, parse_dates=True, squeeze=True)
+print(type(series))
+print(series.head())
