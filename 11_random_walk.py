50136186d55bb150914431c56d104414346e55a7
rodferro <rod_ferro@msn.com>
2018-12-08 12:45:16 -0200

    Reformat code

--- 11_random_walk.py
+++ 11_random_walk.py
@@ -8,7 +8,7 @@ random_walk = list()
 random_walk.append(-1 if random() < 0.5 else 1)
 for i in range(1, 1000):
   movement = -1 if random() < 0.5 else 1
-  value = random_walk[i-1] + movement
+  value = random_walk[i - 1] + movement
   random_walk.append(value)
 pyplot.plot(random_walk)
 pyplot.show()
--- 11_random_walk_autocorrelation.py
+++ 11_random_walk_autocorrelation.py
@@ -9,7 +9,7 @@ random_walk = list()
 random_walk.append(-1 if random() < 0.5 else 1)
 for i in range(1, 1000):
   movement = -1 if random() < 0.5 else 1
-  value = random_walk[i-1] + movement
+  value = random_walk[i - 1] + movement
   random_walk.append(value)
 autocorrelation_plot(random_walk)
 pyplot.show()
